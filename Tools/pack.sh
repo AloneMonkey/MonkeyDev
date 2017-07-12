bb582535f23c5054a309dabf60cd752ed5c18b15
NetEase <netease@AloneMonkey.local>
2017-07-12 20:59:12 +0800

    add monkey app support

---
+++ Tools/pack.sh
@@ -0,0 +1,65 @@
+echo "packing..."
+# environment
+TEMP_PATH="${SRCROOT}/$TARGET_NAME/tmp"
+MONKEYDEV_TOOLS="/opt/MonkeyDev/Tools/"
+DEMOTARGET_APP_PATH="/opt/MonkeyDev/Resource/TargetApp.app"
+OPTOOL="$MONKEYDEV_TOOLS/optool"
+FRAMEWORKS_TO_INJECT_PATH="/opt/MonkeyDev/Frameworks/"
+CUSTOM_DISPLAY_NAME=$(/usr/libexec/PlistBuddy -c "Print CFBundleDisplayName"  "${SRCROOT}/$TARGET_NAME/Info.plist")
+CUSTOM_BUNDLE_ID="$PRODUCT_BUNDLE_IDENTIFIER"
+
+rm -rf "$TEMP_PATH" || true
+mkdir -p "$TEMP_PATH" || true
+
+#deal ipa or app
+TARGET_IPA_PATH=$(find "$SRCROOT/$TARGET_NAME/TargetApp" -type f | grep ".ipa$" | head -n 1)
+
+echo "target ipa:$TARGET_IPA_PATH"
+
+if [[ "$TARGET_IPA_PATH" != "" ]]; then
+	unzip -oqq "$TARGET_IPA_PATH" -d "$TEMP_PATH"
+	TEMP_APP_PATH=$(set -- "$TEMP_PATH/Payload/"*.app; echo "$1")
+	cp -rf "$TEMP_APP_PATH" "$SRCROOT/$TARGET_NAME/TargetApp/"
+fi
+
+BUILD_APP_PATH="$BUILT_PRODUCTS_DIR/$TARGET_NAME.app"
+TARGET_APP_PATH=$(find "$SRCROOT/$TARGET_NAME/TargetApp" -type d | grep ".app$" | head -n 1)
+
+echo "target app:$TARGET_APP_PATH"
+if [[ "$TARGET_APP_PATH" != "" ]]; then
+	cp -rf "$TARGET_APP_PATH/" "$BUILD_APP_PATH/"
+	echo "copy $TARGET_APP_PATH to $BUILD_APP_PATH"
+else
+	cp -rf "$DEMOTARGET_APP_PATH/" "$BUILD_APP_PATH/"
+fi
+
+# copy default framewrok
+TARGET_APP_FRAMEWORKS_PATH="$BUILD_APP_PATH/Frameworks/"
+cp -rf "$FRAMEWORKS_TO_INJECT_PATH" "$TARGET_APP_FRAMEWORKS_PATH"
+
+# Inject the Dynamic Lib
+APP_BINARY=`plutil -convert xml1 -o - $BUILD_APP_PATH/Info.plist|grep -A1 Exec|tail -n1|cut -f2 -d\>|cut -f1 -d\<`
+
+"$OPTOOL" install -c load -p "@executable_path/Frameworks/lib""$TARGET_NAME""Dylib.dylib" -t "$BUILD_APP_PATH/$APP_BINARY"
+
+chmod +x "$BUILD_APP_PATH/$APP_BINARY"
+
+# remove Plugin an Watch
+rm -rf "$BUILD_APP_PATH/PlugIns" || true
+rm -rf "$BUILD_APP_PATH/Watch" || true
+
+# Update Info.plist for Target App
+if [[ "$CUSTOM_DISPLAY_NAME" != "" ]]; then
+	/usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName $CUSTOM_DISPLAY_NAME" "$BUILD_APP_PATH/Info.plist"
+fi
+/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $PRODUCT_BUNDLE_IDENTIFIER" "$BUILD_APP_PATH/Info.plist"
+
+#codesign
+if [ -d "$TARGET_APP_FRAMEWORKS_PATH" ]; then
+for FRAMEWORK in "$TARGET_APP_FRAMEWORKS_PATH/"*
+do
+    FILENAME=$(basename $FRAMEWORK)
+    /usr/bin/codesign --force --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$FRAMEWORK"
+done
+fi
+
---
+++ frameworks/RevealServer.framework/Headers/RevealServer.h
@@ -0,0 +1,19 @@
+//
+//  RevealServer.h
+//  RevealServer
+//
+//  Created by Tony Arnold on 25/11/2015.
+//  Copyright © 2015 Itty Bitty Apps, Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RevealServer.
+FOUNDATION_EXPORT double RevealServerVersionNumber;
+
+//! Project version string for RevealServer.
+FOUNDATION_EXPORT const unsigned char RevealServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RevealServer/PublicHeader.h>
+
+
