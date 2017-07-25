7955fe58dd4e3773fd55d3c563956a9fe7072a30
AloneMonkey <liupeiqing1993@163.com>
2017-07-25 21:52:23 +0800

    remove source cocoapods script

--- Tools/pack.sh
+++ Tools/pack.sh
@@ -99,10 +99,10 @@ done
 fi
 
 #cocoapods
-if [[ -f "${SRCROOT}/Pods/Target Support Files/Pods-MonkeyPodDylib/Pods-MonkeyPodDylib-frameworks.sh" ]]; then
-	source "${SRCROOT}/Pods/Target Support Files/Pods-MonkeyPodDylib/Pods-MonkeyPodDylib-frameworks.sh"
-fi
+# if [[ -f "${SRCROOT}/Pods/Target Support Files/Pods-MonkeyPodDylib/Pods-MonkeyPodDylib-frameworks.sh" ]]; then
+# 	source "${SRCROOT}/Pods/Target Support Files/Pods-MonkeyPodDylib/Pods-MonkeyPodDylib-frameworks.sh"
+# fi
 
-if [[ -f "${SRCROOT}/Pods/Target Support Files/Pods-MonkeyPodDylib/Pods-MonkeyPodDylib-resources.sh" ]]; then
-	source "${SRCROOT}/Pods/Target Support Files/Pods-MonkeyPodDylib/Pods-MonkeyPodDylib-resources.sh"
-fi
+# if [[ -f "${SRCROOT}/Pods/Target Support Files/Pods-MonkeyPodDylib/Pods-MonkeyPodDylib-resources.sh" ]]; then
+# 	source "${SRCROOT}/Pods/Target Support Files/Pods-MonkeyPodDylib/Pods-MonkeyPodDylib-resources.sh"
+# fi
