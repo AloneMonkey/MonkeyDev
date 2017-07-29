a7f86a3d4297be43993dc1daa451e7f87bf8f34c
AloneMonkey <liupeiqing1993@163.com>
2017-07-29 21:57:04 +0800

    add class-dump and restore-symbol

--- Tools/pack.sh
+++ Tools/pack.sh
@@ -1,3 +1,20 @@
+MONKEYDEV_PATH="/opt/MonkeyDev"
+TEMP_PATH="${SRCROOT}/$TARGET_NAME/tmp"
+RESTORE_SYMBOL_TOOL="$MONKEYDEV_PATH/bin/restore-symbol"
+CLASS_DUMP_TOOL="$MONKEYDEV_PATH/bin/class-dump"
+
+function panic() # args: exitCode, message...
+{
+	local exitCode=$1
+	set +e
+	
+	shift
+	[[ "$@" == "" ]] || \
+		echo "$@" >&2
+
+	exit $exitCode
+}
+
 function codesign()
 {
     for file in `ls $1`;
@@ -17,10 +34,80 @@ function codesign()
     done
 }
 
+function checkApp(){
+	TARGET_APP_PATH="$1"
+	MACH_O_FILE_NAME=`plutil -convert xml1 -o - $TARGET_APP_PATH/Info.plist|grep -A1 Exec|tail -n1|cut -f2 -d\>|cut -f1 -d\<`
+	MACH_O_FILE_PATH="$TARGET_APP_PATH/$MACH_O_FILE_NAME"
+	ARMV7=false
+ 	ARM64=false
+ 	FAT_FILE=false
+ 	[[ $(lipo -info $MACH_O_FILE_PATH | grep armv7) == "" ]] || ARMV7=true
+ 	[[ $(lipo -info $MACH_O_FILE_PATH | grep arm64) == "" ]] || ARM64=true
+ 	[[ $(lipo -info $MACH_O_FILE_PATH | grep "Non-fat file") != "" ]] || FAT_FILE=true
+ 	echo "has arm7 arch? $ARMV7"
+ 	echo "has arm64 arch? $ARM64"
+ 	echo "is fat file? $FAT_FILE"
+ 	if [[ ! ARMV7 && ! ARM64 ]]; then
+  		panic 1 "The target does not contain armv7 or arm64 arch!!!"
+ 	fi
+ 	decrypted_num=$(otool -l $MACH_O_FILE_PATH | grep "cryptid 0" | wc -l | tr -d " ")
+ 	echo "decrypted arch num? $decrypted_num"
+ 	if [[ "$decrypted_num" == "0" ]]; then
+ 		panic 1 "can't find decrypted arch!!!"
+ 	fi
+ 	#class_dump
+ 	if [[ ! -f "$TARGET_APP_PATH"/md_class_dump ]] && [[ "${MONKEYDEV_CLASS_DUMP}" == "YES" ]]; then
+ 		TARGET_DUMP_DIR="${SRCROOT}/$TARGET_NAME/$MACH_O_FILE_NAME"_Headers
+ 		if [[ -d "$TARGET_DUMP_DIR" ]]; then
+ 			rm -rf "$TARGET_DUMP_DIR" || true
+ 		fi
+ 		mkdir -p "$TARGET_DUMP_DIR"
+
+ 		if [[ "$FAT_FILE" && "$ARMV7" && "$ARM64" && "$decrypted_num" == "1" ]]; then
+ 			decrypted_arch="arm64"
+ 			lipo -thin armv7 $MACH_O_FILE_PATH -o $TEMP_PATH/"$MACH_O_FILE_NAME"_armv7
+			lipo -thin arm64 $MACH_O_FILE_PATH -o $TEMP_PATH/"$MACH_O_FILE_NAME"_arm64
+ 			[[ $(otool -l $TEMP_PATH/"$MACH_O_FILE_NAME"_armv7 | grep "cryptid 0") == "" ]] || decrypted_arch="armv7"
+ 			[[ $(otool -l $TEMP_PATH/"$MACH_O_FILE_NAME"_arm64 | grep "cryptid 0") == "" ]] || decrypted_arch="arm64"
+ 			echo "current decrypted arch: $decrypted_arch"
+ 			"$CLASS_DUMP_TOOL" "$MACH_O_FILE_PATH" --arch "$decrypted_arch" -H -o "$TARGET_DUMP_DIR"
+ 		else
+ 			"$CLASS_DUMP_TOOL" "$MACH_O_FILE_PATH" -H -o "$TARGET_DUMP_DIR"
+ 		fi
+ 		echo "finsih_class_dump" >> "$TARGET_APP_PATH"/md_class_dump
+ 	fi
+ 	#restore_symbol
+ 	if [[ ! -f "$TARGET_APP_PATH"/md_restore_symbol ]] && [[ "${MONKEYDEV_RESTORE_SYMBOL}" == "YES" ]]; then
+ 		if "$FAT_FILE"; then
+ 			if "$ARMV7" && "$ARM64"; then
+ 				lipo -thin armv7 $MACH_O_FILE_PATH -o $TEMP_PATH/"$MACH_O_FILE_NAME"_armv7
+				lipo -thin arm64 $MACH_O_FILE_PATH -o $TEMP_PATH/"$MACH_O_FILE_NAME"_arm64
+				"$RESTORE_SYMBOL_TOOL" $TEMP_PATH/$"$MACH_O_FILE_NAME"_armv7 -o $TEMP_PATH/$"$MACH_O_FILE_NAME"_armv7_with_symbol
+				"$RESTORE_SYMBOL_TOOL" $TEMP_PATH/$"$MACH_O_FILE_NAME"_arm64 -o $TEMP_PATH/$"$MACH_O_FILE_NAME"_arm64_with_symbol
+				lipo -create $TEMP_PATH/"$MACH_O_FILE_NAME"_armv7_with_symbol $TEMP_PATH/"$MACH_O_FILE_NAME"_arm64_with_symbol -o $TEMP_PATH/"$MACH_O_FILE_NAME"_with_symbol
+				cp -rf $TEMP_PATH/"$MACH_O_FILE_NAME"_with_symbol $MACH_O_FILE_PATH
+			elif "$ARMV7"; then
+				lipo -thin armv7 $MACH_O_FILE_PATH -o $TEMP_PATH/"$MACH_O_FILE_NAME"_armv7
+				"$RESTORE_SYMBOL_TOOL" $TEMP_PATH/"$MACH_O_FILE_NAME"_armv7 -o $TEMP_PATH/"$MACH_O_FILE_NAME"_armv7_with_symbol
+	 			cp -rf $TEMP_PATH/"$MACH_O_FILE_NAME"_armv7_with_symbol $MACH_O_FILE_PATH
+ 			elif "$ARM64"; then
+ 				lipo -thin arm64 $MACH_O_FILE_PATH -o $TEMP_PATH/"$MACH_O_FILE_NAME"_arm64
+ 				"$RESTORE_SYMBOL_TOOL" $TEMP_PATH/"$MACH_O_FILE_NAME"_arm64 -o $TEMP_PATH/"$MACH_O_FILE_NAME"_arm64_with_symbol
+	 			cp -rf $TEMP_PATH/"$MACH_O_FILE_NAME"_arm64_with_symbol $MACH_O_FILE_PATH
+ 			fi
+	 	elif "$ARMV7"; then
+	 		"$RESTORE_SYMBOL_TOOL" "$MACH_O_FILE_PATH" -o $TEMP_PATH/"$MACH_O_FILE_NAME"_armv7_with_symbol
+	 		cp -rf $TEMP_PATH/"$MACH_O_FILE_NAME"_armv7_with_symbol $MACH_O_FILE_PATH
+	 	elif "$ARM64"; then
+	 		"$RESTORE_SYMBOL_TOOL" "$MACH_O_FILE_PATH" -o $TEMP_PATH/"$MACH_O_FILE_NAME"_arm64_with_symbol
+	 		cp -rf $TEMP_PATH/"$MACH_O_FILE_NAME"_arm64_with_symbol $MACH_O_FILE_PATH
+	 	fi
+	 	echo "finsih_restore_symbol" >> "$TARGET_APP_PATH"/md_restore_symbol
+ 	fi
+}
+
 echo "packing..."
 # environment
-MONKEYDEV_PATH="/opt/MonkeyDev"
-TEMP_PATH="${SRCROOT}/$TARGET_NAME/tmp"
 MONKEYDEV_TOOLS="$MONKEYDEV_PATH/Tools/"
 DEMOTARGET_APP_PATH="$MONKEYDEV_PATH/Resource/TargetApp.app"
 OPTOOL="$MONKEYDEV_PATH/bin/optool"
@@ -33,11 +120,10 @@ rm -rf "$TEMP_PATH" || true
 mkdir -p "$TEMP_PATH" || true
 
 #deal ipa or app
+TARGET_APP_PATH=$(find "$SRCROOT/$TARGET_NAME/TargetApp" -type d | grep ".app$" | head -n 1)
 TARGET_IPA_PATH=$(find "$SRCROOT/$TARGET_NAME/TargetApp" -type f | grep ".ipa$" | head -n 1)
 
-echo "target ipa:$TARGET_IPA_PATH"
-
-if [[ "$TARGET_IPA_PATH" != "" ]]; then
+if [[ "$TARGET_APP_PATH" == "" ]] && [[ "$TARGET_IPA_PATH" != "" ]]; then
 	unzip -oqq "$TARGET_IPA_PATH" -d "$TEMP_PATH"
 	TEMP_APP_PATH=$(set -- "$TEMP_PATH/Payload/"*.app; echo "$1")
 	cp -rf "$TEMP_APP_PATH" "$SRCROOT/$TARGET_NAME/TargetApp/"
@@ -49,12 +135,11 @@ rm -rf "$BUILD_APP_PATH" || true
 mkdir -p "$BUILD_APP_PATH" || true
 
 TARGET_APP_PATH=$(find "$SRCROOT/$TARGET_NAME/TargetApp" -type d | grep ".app$" | head -n 1)
-
-echo "target app:$TARGET_APP_PATH"
 if [[ "$TARGET_APP_PATH" != "" ]]; then
+	checkApp "$TARGET_APP_PATH" 
 	cp -rf "$TARGET_APP_PATH/" "$BUILD_APP_PATH/"
 	echo "copy $TARGET_APP_PATH to $BUILD_APP_PATH"
-else
+else 
 	cp -rf "$DEMOTARGET_APP_PATH/" "$BUILD_APP_PATH/"
 fi
 
@@ -127,4 +212,4 @@ fi
 
 if [[ -f "${SRCROOT}/Pods/Target Support Files/Pods-""$TARGET_NAME""Dylib/Pods-""$TARGET_NAME""Dylib-resources.sh" ]]; then
 	source "${SRCROOT}/Pods/Target Support Files/Pods-""$TARGET_NAME""Dylib/Pods-""$TARGET_NAME""Dylib-resources.sh"
-fi 
+fi
